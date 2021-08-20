8389be53f956872565948688410df81c41387853
Aryamannn <mishraaryaman2003@gmail.com>
2021-08-20 18:40:51 +0530

    Stopwatch

---
+++ app/src/main/java/www/app/stopwatch/MainActivity.java
@@ -0,0 +1,43 @@
+package www.app.stopwatch;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+    }
+
+    public void startChronometer (View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer (View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer (View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+}
